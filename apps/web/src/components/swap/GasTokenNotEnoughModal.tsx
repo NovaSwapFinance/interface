221e5716452033db4a135a3340d01554c82954f4
MickWang <1244134672@qq.com>
2024-07-26 17:45:38 +0800

    update ui for gas not enough modal

--- apps/web/src/components/swap/GasTokenNotEnoughModal.tsx
+++ apps/web/src/components/swap/GasTokenNotEnoughModal.tsx
@@ -4,14 +4,16 @@ import { AutoColumn } from "components/Column";
 import { Text } from "rebass";
 import { ButtonPrimary } from "components/Button";
 import { Trans } from "i18n";
-import styled from "styled-components";
-import { InterfaceTrade } from "state/routing/types";
+import styled, { useTheme } from "styled-components";
+import { EstimatePaymasterGasUsd } from "./SwapGasTokenDropdown";
+import AlertTriangleFilled from "components/Icons/AlertTriangleFilled";
+import { DialogButtonType, DialogContent } from "components/Dialog/Dialog";
+import { ColumnCenter } from "components/Column";
 
 interface IProps {
   open: boolean;
   gasTokenAmount: CurrencyAmount;
   onCancel: () => void;
-  trade?: InterfaceTrade;
 }
 const Wrapper = styled.div`
   width: 100%;
@@ -26,6 +28,18 @@ const InfoText = styled(Text)`
   font-size: 14px;
   line-height: 20px;
   text-align: center;
+  margin-bottom: 15px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  background-color: ${({ theme }) => theme.surface3};
+  width: 48px;
+  height: 48px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  margin-bottom: 20px;
 `;
 
 const StyledButton = styled(ButtonPrimary)`
@@ -38,22 +52,52 @@ export default function GasTokenNotEnoughModal({
   isOpen,
   onCancel,
   gasTokenAmount,
-  trade,
 }: IProps) {
+  const theme = useTheme();
+
   const gasTokenSymbol = gasTokenAmount?.currency.symbol;
-  const gasUsd = trade?.totalGasUseEstimateUSD ?? 0;
+  const gasUsd = EstimatePaymasterGasUsd;
   const gasTokenAmountStr = gasTokenAmount?.toExact();
   return (
-    <Modal isOpen={isOpen} onDismiss={onCancel}>
+    <Modal
+      isOpen={isOpen}
+      $scrollOverlay
+      onDismiss={onCancel}
+      maxHeight={90}
+      slideIn
+    >
       <Wrapper>
-        <InfoText>
-          Not enough {gasTokenSymbol} for gas. Gas fee: {gasTokenAmountStr}
-          {gasTokenSymbol} (${gasUsd})
-        </InfoText>
-
-        <StyledButton onClick={onCancel}>
-          <Trans>Close</Trans>
-        </StyledButton>
+        <DialogContent
+          isVisible={true}
+          icon={
+            <AlertTriangleFilled
+              data-testid="pending-modal-failure-icon"
+              fill={theme.neutral2}
+              size="24px"
+            />
+          }
+          title={<Trans>Swap failed</Trans>}
+          description={
+            <Trans>Try adjusting from amount to a lower value.</Trans>
+          }
+          body={
+            <ColumnCenter gap="md">
+              <InfoText style={{ marginBottom: 20 }}>
+                Not enough {gasTokenSymbol} for gas. Gas fee:{" "}
+                {gasTokenAmountStr}
+                {gasTokenSymbol} (${gasUsd})
+              </InfoText>
+            </ColumnCenter>
+          }
+          buttonsConfig={{
+            left: {
+              type: DialogButtonType.Accent,
+              title: <Trans>Close</Trans>,
+              onClick: onCancel,
+            },
+          }}
+          onCancel={onCancel}
+        />
       </Wrapper>
     </Modal>
   );
